2547fb182a84798ef257d6f71f0533feed60ec69
ALeaxanderaz04 <125475633+ALeaxanderaz04@users.noreply.github.com>
2023-11-15 20:06:00 -0600

    Nuevo Appbar

--- Proyecto Ingreso FarmSiman/sakai-js/layout/AppFooter.js
+++ Proyecto Ingreso FarmSiman/sakai-js/layout/AppFooter.js
@@ -6,9 +6,9 @@ const AppFooter = () => {
 
     return (
         <div className="layout-footer">
-            <img src={`https://i.pinimg.com/564x/44/8f/67/448f67cce58679194f8ed6d7a2fa8699.jpg`} alt="Logo" height="20" className="mr-2" />
+            
             Por
-            <span className="font-medium ml-2">Servicios Publicitarios Monsoon</span>
+            <span className="font-medium ml-2">Viajes FARM</span>
         </div>
     );
 };
